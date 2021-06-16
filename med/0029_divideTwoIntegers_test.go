1d54f8a05f008a76e2038430b9515baa0f659af5
Rishi Maharaj <rishi.maharaj@pwc.com>
2021-06-15 17:02:17 -0700

    Ignoring long running tests

--- med/0029_divideTwoIntegers_test.go
+++ med/0029_divideTwoIntegers_test.go
@@ -37,6 +37,9 @@ func Test_divide(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if testing.Short() && tt.name == "-2147483648/-1=2147483647" || tt.name == "-2147483648/1=2147483647" {
+				t.Skip("skipping test in short mode.")
+			}
 			if got := divide(tt.args.dividend, tt.args.divisor); got != tt.want {
 				t.Errorf("divide() = %v, want %v", got, tt.want)
 			}
